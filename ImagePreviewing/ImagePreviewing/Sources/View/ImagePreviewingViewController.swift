b87498ce518f9568cb7877fc0a115db32fd9411e
Sergey <sjsoad666@yandex.ru>
2018-06-23 11:45:10 +0300

    no message

--- ImagePreviewing/ImagePreviewing/Sources/View/ImagePreviewingViewController.swift
+++ ImagePreviewing/ImagePreviewing/Sources/View/ImagePreviewingViewController.swift
@@ -24,7 +24,7 @@ open class ImagePreviewingViewController: UIViewController, ImagePreviewingInter
         super.init(nibName: nibNameOrNil, bundle: bundle)
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
